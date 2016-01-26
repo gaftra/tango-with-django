fcbbd73dfb2cac4ee409fc2bce607b6ab87acf76
Stephen Brown <1106679B@student.gla.ac.uk>
2016-01-26 12:14:46 +0000

    Chapter 5 basics done

--- tangowithdjango/settings.py
+++ tangowithdjango/settings.py
@@ -13,6 +13,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
