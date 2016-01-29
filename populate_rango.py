d4d5ac090bc1004a851704ef6da0882b4314e183
Stephen Brown <1106679B@student.gla.ac.uk>
2016-01-29 16:18:58 +0000

    Chapter 7 exercises finished

--- populate_rango.py
+++ populate_rango.py
@@ -11,7 +11,8 @@ def populate():
 	
 	add_page(cat=python_cat,
 		title="Official Python Tutorial",
-		url="http://docs.python.org/2/tutorial")
+		url="http://docs.python.org/2/tutorial",
+		views=200)
 		
 	add_page(cat=python_cat,
 		title="How to Think like a Computer Scientist",
@@ -29,21 +30,25 @@ def populate():
 		
 	add_page(cat=django_cat,
 		title="Django Rocks",
-		url="http://www.djangorocks.com/")
+		url="http://www.djangorocks.com/",
+		views=100)
 		
 	add_page(cat=django_cat,
 		title="How to Tango with Django",
-		url="http://www.tangowithdjango.com/")
+		url="http://www.tangowithdjango.com/",
+		views=50)
 		
 	frame_cat = add_cat("Other Frameworks",32,16)
 	
 	add_page(cat=frame_cat,
 		title="Bottle",
-		url="http://bottlepy.org/docs/dev/")
+		url="http://bottlepy.org/docs/dev/",
+		views=49)
 		
 	add_page(cat=frame_cat,
 		title="Flask",
-		url="httP://flask.pocoo.org")
+		url="httP://flask.pocoo.org",
+		views=2)
 		
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
