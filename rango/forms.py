7cd57aadcdc0ad086dbb91c52851250f668e4079
Stephen Brown <1106679B@student.gla.ac.uk>
2016-02-11 09:44:45 +0000

    Chapter 8 exercises complete

--- rango/forms.py
+++ rango/forms.py
@@ -22,7 +22,7 @@ class PageForm(forms.ModelForm):
 		
 	def clean(self):
 		cleaned_data = self.cleaned_data
-		url = cleane_data.get('url')
+		url = cleaned_data.get('url')
 		
 		if url and not url.startswith('http://'):
 			url = 'http://' + url
--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
 
@@ -17,6 +18,7 @@ def category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
+        context_dict['category_slug'] = category_name_slug
 
         pages = Page.objects.filter(category=category)
 
@@ -43,6 +45,34 @@ def add_category(request):
 		form = CategoryForm()
 		
 	return render(request, 'rango/add_category.html', {'form': form})
+	
+def add_page(request, category_name_slug):
+
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+		
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request, category_name_slug)
+		
+		else:
+			print form.errors
+	
+	else:
+		form = PageForm()
+		
+	context_dict = {'form': form, 'category': cat, 'category_name_slug': category_name_slug}
+	
+	return render(request, 'rango/add_page.html', context_dict)
 	
 def about(request):
 	context_dict = {'':''}
