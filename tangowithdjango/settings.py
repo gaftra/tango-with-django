75e0f0c337cb68ab88049bf49fa58c60060ebdfe
Stephen Brown <1106679B@student.gla.ac.uk>
2016-02-11 10:31:59 +0000

    Chapter 9 basics done

--- tangowithdjango/settings.py
+++ tangowithdjango/settings.py
@@ -16,6 +16,8 @@ TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = '/rango/login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
