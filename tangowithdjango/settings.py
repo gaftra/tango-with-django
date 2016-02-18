cbea8b87c9207872f07124e4a6b546a51ba40d1f
Stephen Brown <1106679B@student.gla.ac.uk>
2016-02-18 15:22:35 +0000

    Chapter 12 Basics Done

--- tangowithdjango/settings.py
+++ tangowithdjango/settings.py
@@ -31,6 +31,12 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Registration variables
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
 
 # Application definition
 
@@ -42,6 +48,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'rango',
+	'registration',
 )
 
 MIDDLEWARE_CLASSES = (
