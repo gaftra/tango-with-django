85c798ff06fe90a72051c0097435cb09e638ec87
Stephen Brown <1106679B@student.gla.ac.uk>
2016-02-18 15:40:15 +0000

    Chapter 12 exercises done

--- tangowithdjango/urls.py
+++ tangowithdjango/urls.py
@@ -16,9 +16,8 @@ urlpatterns = patterns('',
 	url(r'^rango/', include('rango.urls')),
 	url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
 	url(r'^accounts/', include('registration.backends.simple.urls')),
-	url(r'^password/', include('registration.backends.simple.urls')),
 )
-
+	
 if settings.DEBUG:
 	urlpatterns += patterns(
 		'django.views.static',
